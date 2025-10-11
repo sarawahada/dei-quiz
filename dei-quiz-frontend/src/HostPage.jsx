a0c8d3ceed9f5acb4142abbbbabd10c374393da0
sarawahada <61452171+sarawahada@users.noreply.github.com>
2025-10-12 00:45:05 +0200

    Fix QR code display on host page

--- dei-quiz-frontend/src/HostPage.jsx
+++ dei-quiz-frontend/src/HostPage.jsx
@@ -52,7 +52,7 @@ export default function HostPage() {
       {roomId && (
         <div>
           <p>Room ID: {roomId}</p>
-          <QRCodeCanvas value={`https://dei-quiz1.onrender.com?room=${roomId}`} />
+            <QRCode value={`https://dei-quiz1.onrender.com/player?room=${roomId}`} />
           <h3>Players Joined:</h3>
           <ul>
             {players.map((p, idx) => (
