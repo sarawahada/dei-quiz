4de809499a319e4a74237e78bb1ec1e46e1b8173
sarawahada <61452171+sarawahada@users.noreply.github.com>
2025-10-12 16:03:26 +0200

    Updated PlayerPage.jsx, added react-icons, and fixed Vite build issues

--- dei-quiz-frontend/src/App.jsx
+++ dei-quiz-frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import HostPage from "./HostPage";
 import PlayerPage from "./PlayerPage";
-
+import CharacterPage from "./CharacterPage";
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HostPage />} />
       <Route path="/play/:roomId" element={<PlayerPage />} />
-      <Route path="/character/:name" element={<Character />} />
+      <Route path="/character/:name" element={<CharacterPage />} />
     </Routes>
   );
 }
